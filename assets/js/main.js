6bb09f234abb1c9a5240c15555cde17e7f5f09ed
Shahidulis <shahidulislam47326@gmail.com>
2024-09-19 19:09:27 +0600

    update now

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+// navBar section
+const navBer = document.querySelector(".navber-nav");
+const menuBar = document.getElementById("menu-btn");
+const closesBtn = document.getElementById("closes-navber");
+
+menuBar.addEventListener("click", function () {
+  navBer.classList.add("active");
+  console.log(navBer);
+});
+
+closesBtn.addEventListener("click", function () {
+  navBer.classList.remove("active");
+});
+
+
+
+
+
 // / Odometer Init
 $(document).ready(function () {
   $(".odometer").each(function () {
